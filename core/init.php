8323a582acaac4069dc2aae2d97435fb328bb71c
Joe Ainsworth <joe.ainsworth@me.com>
2016-03-20 22:17:49 +0000

    Set-up structure and globals

---
+++ core/init.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'username' => 'joeainsworth',
+        'password' => '',
+        'db' => 'website'
+    ],
+    'remember' => [
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
+    ],
+    'session' => [
+        'session_name' => 'user'
+    ]
+];
+
+spl_autoload_register(function($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/sanitize.php';
